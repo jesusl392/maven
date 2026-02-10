2e006e48889a6e9caf2581f8f996001ae5e08e3f
jesusl392 <jesusslozano@ucundinamarca.edu.co>
2026-02-10 13:31:26 -0500

    trabajo POO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,18 @@
+import org.example.Persona;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("hola mundo");
+        //crear persona desde constructor vacio
+        Persona p1 = new Persona();
+        p1.setNombre("juanito");
+        p1.setEdad(26);
+        p1.setGenero("masculino");
+
+        //segunda persona creada usando constructor con todos los parametros
+        Persona p2 = new Persona("maria",23,"femenino");
+
+        //mostrar persona
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
     }
 }
